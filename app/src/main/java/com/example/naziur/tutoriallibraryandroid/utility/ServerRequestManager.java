fa98cfb55ee30e65839b6d1b827f65264a9d3c15
rahmann5 <rahmann5@aston.ac.uk>
2018-03-13 20:06:37 +0000

    In the process of making first server request

--- app/src/main/java/com/example/naziur/tutoriallibraryandroid/utility/ServerRequestManager.java
+++ app/src/main/java/com/example/naziur/tutoriallibraryandroid/utility/ServerRequestManager.java
@@ -1,5 +1,18 @@
 package com.example.naziur.tutoriallibraryandroid.utility;
 
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Hashtable;
+import java.util.Map;
+
 /**
  * Created by Hamidur on 13/03/2018.
  */
@@ -19,6 +32,33 @@ public class ServerRequestManager {
         onRequestCompleteListener = listener;
     }
 
+    public static void getTutorials(Context context){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, END_POINT + "all_tutorials", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                //Creating parameters
+                Map<String, String> params = new Hashtable<String, String>();
+                params.put(APP_VALUE, APP_KEY);
+                //returning parameters
+                return params;
+            }
+        };
+
+        //Creating a Request Queue
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+
+        //Adding request to the queue
+        requestQueue.add(stringRequest);
+    }
 
 }
